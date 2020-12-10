8b63bbc036908a5e63a5df3df4a242edb3bb49d8
Aleks <nikolic.aleks90@gmail.com>
2020-12-10 16:09:38 -0500

    First commit - basic file setup, some DB connection config going on

---
+++ src/main/java/com/reavture/repositories/NTCBankMemoryDAO.java
@@ -0,0 +1,5 @@
+package com.reavture.repositories;
+
+public class NTCBankMemoryDAO {
+
+}
---
+++ src/main/java/com/revature/launcher/NTCLauncher.java
@@ -0,0 +1,9 @@
+package com.revature.launcher;
+
+public class NTCLauncher {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+
+}
