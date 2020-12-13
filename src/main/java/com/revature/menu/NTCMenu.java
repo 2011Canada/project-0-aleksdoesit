ac8e23cdc93d77f5862f7f111fb94204da7d608b
Aleks <nikolic.aleks90@gmail.com>
2020-12-12 20:00:51 -0500

    menu is coming along!

--- src/main/java/com/revature/menu/NTCMenu.java
+++ src/main/java/com/revature/menu/NTCMenu.java
@@ -7,41 +7,68 @@ import java.util.Scanner;
 import com.revature.models.Displayable;
 import com.revature.services.CustomerService;
 
-public class NTCMenu implements Displayable {
+public class NTCMenu {
 	
 	private CustomerService cs;
+
 	
-	Scanner userIn;
+//	List<Displayable> lines;
 	
-	List<Displayable> lines;
-	
-	
-	public loginMenu(CustomerService cs) {
+	public void welcomeMenu() {
 		
-			this.cs = cs;
-			this.lines = new ArrayList<Displayable>();
+		Scanner userIn = new Scanner(System.in);
+		
+		boolean validSelection = false;
+		
+		int userChoice = -10;
+		
+		do {
 			
-			lines.add(()->"WELCOME TO NEW TORONTO CREDIT\" + \"\\n\" + \"Please press Enter to login");
-			this.userIn = new Scanner(System.in);
+			System.out.println("WELCOME TO NEW TORONTO CREDIT");
+			System.out.println("Please choose one of the following options: ");
+			System.out.println("1. Existing Customer Login");
+			System.out.println("2. New Customer Registration");
+			System.out.println("3. Employee Login");
+			System.out.println("0. Exit Application");
+			
+			
+			try {	
+				userChoice = userIn.nextInt();
+				
+				if(userChoice >= 0 && userChoice < 4) {
+					
+					System.out.println("worked?");
+					
+				} else {
+					
+					System.out.println("Please make a valid selection.");
+					
+					validSelection = false;
+					userIn.next();
+					
+				}
+			} catch(InvalidChoiceSelection e) {
+				
+			}
+			
+			
+		} while (!validSelection) ;
+			
+			System.out.println(userChoice);
+			
+		}
 	}
 	
-	@Override
-	public String display() {
-		String display = "Press Enter to login";
-		return display;
-	}
 	
 	
-	public void manageUserLogin() {
+//	public void manageUserLogin() {
 //		create a different menu but this works well for validation
 //		if else blocks, but we'll still use this method to check login credentials disregard rest.
-		String dummy = this.userIn.nextLine();
-		System.out.println("Please enter your Account Name");
-		String accountName = this.userIn.nextLine();
-		System.out.println("Please enter your Password");
-		String password = this.userIn.nextLine();
-		this.cs.login(account_name, password).display();
-		
-	}
-
-}
+//		String dummy = this.userIn.nextLine();
+//		System.out.println("Please enter your Account Name");
+//		String accountName = this.userIn.nextLine();
+//		System.out.println("Please enter your Password");
+//		String password = this.userIn.nextLine();
+//		this.cs.login(account_name, password).display();
+//		
+//	}
---
+++ src/main/java/com/revature/menu/NTCMenuDebugger.java
@@ -0,0 +1,13 @@
+package com.revature.menu;
+
+public class NTCMenuDebugger {
+
+	public static void main(String[] args) {
+		
+		NTCMenu menu = new NTCMenu();
+		
+		menu.welcomeMenu();
+
+	}
+
+}
